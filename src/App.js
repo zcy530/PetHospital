969fc14a367cbeae48b1411548946c285d7a839e
Bella Liang <bellal@BelladeMacBook-Pro-5.local>
2023-03-28 17:45:43 +0800

    修改部署及路由

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import DutyLearn from './pages_front/learnDuty/dutyLearn.tsx';
 import Detail from "./pages_front/learnCase/details.tsx";
 import Guide from './pages_front/guide/guide.tsx';
 import Exam from './pages_front/exam/exam.tsx';
-import NavBar from "./Components/Navbar.tsx";
-import Footer from "./Components/footer.tsx";
+import NavBar from "./components/Navbar.tsx";
+import Footer from "./components/footer.tsx";
 import Role from './pages_front/learnDuty/role/newRole.js'
 import SystemManage from "./pages_back/systemManage.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';

e8cace5bf6940ac8a612a97146949855fe8aa90e
anti177 <1803697047@qq.com>
2023-04-21 08:53:26 +0800

    修改流程图片url参数，加速加载

--- src/pages_front/learnDuty/role/flowContent.js
+++ src/pages_front/learnDuty/role/flowContent.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './flowContent.css'
 
 function flowContent({name, imgUrl, content}) {
+    const  url = imgUrl + '?x-oss-process=image/resize,m_lfit,w_800,h_800';
     return (
         <div className='flowContent'>
             <div className='flowPic'>
-                <img src={imgUrl} alt=''/>
+                <img src={url} alt=''/>
             </div>
             <div className = "flowOpe">
                 <div className='flowName'>
